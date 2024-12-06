cad91da9f292cdf08b524b8adec6664d21e2fa92
marvelous <donatusmarvelous09@gmail.com>
2024-12-06 03:49:14 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Demo from "./components/Demo"
+import Hero from "./components/Hero"
+
+
+
+function App() {
+
+
+  return (
+    <>
+<main>
+  <div className="main">
+    <div/>
+  </div>
+
+  <div className="app">
+
+   <Hero/>
+   <Demo/>
+  </div>
+</main>
+
+    </>
+  )
+}
+
+export default App
